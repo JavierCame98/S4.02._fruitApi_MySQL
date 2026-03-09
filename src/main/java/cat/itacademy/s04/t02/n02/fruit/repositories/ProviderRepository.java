e1f40d45b558b3f4ec870c9150252c8ed5c6cab2
Javier Castillo <javiercame98@gmail.com>
2026-03-09 10:22:22 +0100

    fix: adding repository annotation on Provider Repository

--- src/main/java/cat/itacademy/s04/t02/n02/fruit/repositories/ProviderRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n02/fruit/repositories/ProviderRepository.java
@@ -2,9 +2,11 @@ package cat.itacademy.s04.t02.n02.fruit.repositories;
 
 import cat.itacademy.s04.t02.n02.fruit.model.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProviderRepository extends JpaRepository <Provider, Long> {
 
     boolean existsByName (String name);
